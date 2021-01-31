d99e9e0e454775b74209972367d028476443c33d
Vince van Oosten <techhazard@codeforyouand.me>
2021-01-31 15:13:57 +0100

    add variable whitelist to envsubst function

--- envsubst.py
+++ envsubst.py
@@ -47,34 +47,43 @@ def _resolve_var(var_name, default=None):
         return os.environ.get(var_name, default)
 
 
-def _repl_simple_env_var(m):
-    var_name = m.group(1)
-    return _resolve_var(var_name, '')
-
-
-def _repl_extended_env_var(m):
-    var_name = m.group(1)
-    default_spec = m.group(2)
-    if default_spec:
-        default = m.group(4)
-        default = _simple_re.sub(_repl_simple_env_var, default)
-        if m.group(3) == ':-':
-            # use default if var is unset or empty
-            env_var = _resolve_var(var_name)
-            if env_var:
-                return env_var
-            else:
-                return default
-        elif m.group(3) == '-':
-            # use default if var is unset
-            return _resolve_var(var_name, default)
+def _repl_simple_env_var(env_var_list):
+    def inner(regex_match):
+        var_name = regex_match.group(1)
+        if env_var_list is None or var_name in env_var_list:
+            return _resolve_var(var_name, '')
         else:
-            raise RuntimeError('unexpected string matched regex')
-    else:
-        return _resolve_var(var_name, '')
+            return regex_match.group(0)
+    return inner
 
 
-def envsubst(string):
+def _repl_extended_env_var(env_var_list):
+    def inner(regex_match):
+        var_name = regex_match.group(1)
+        if env_var_list is not None and var_name not in env_var_list:
+            return regex_match.group(0)
+        default_spec = regex_match.group(2)
+        if default_spec:
+            default = regex_match.group(4)
+            default = _simple_re.sub(_repl_simple_env_var(env_var_list), default)
+            if regex_match.group(3) == ':-':
+                # use default if var is unset or empty
+                env_var = _resolve_var(var_name)
+                if env_var:
+                    return env_var
+                else:
+                    return default
+            elif regex_match.group(3) == '-':
+                # use default if var is unset
+                return _resolve_var(var_name, default)
+            else:
+                raise RuntimeError('unexpected string matched regex')
+        else:
+            return _resolve_var(var_name, '')
+    return inner
+
+
+def envsubst(string, env_var_list=None):
     """
     Substitute environment variables in the given string
 
@@ -92,13 +101,21 @@ def envsubst(string):
         uses "somestring" only if $FOO is unset
 
     :param str string: A string possibly containing environment variables
+    :param list[str] env_var_list: An optional list of strings containing names of environment variables to replace.
+                                   If not given, all variables will be replaced
     :return: The string with environment variable specs replaced with their values
     """
+    if not isinstance(env_var_list, list):
+        if isinstance(env_var_list, str):
+            env_var_list = [env_var_list]
+        else:
+            env_var_list = None
+
     # handle simple un-bracketed env vars like $FOO
-    a = _simple_re.sub(_repl_simple_env_var, string)
+    a = _simple_re.sub(_repl_simple_env_var(env_var_list), string)
 
     # handle bracketed env vars with optional default specification
-    b = _extended_re.sub(_repl_extended_env_var, a)
+    b = _extended_re.sub(_repl_extended_env_var(env_var_list), a)
     return b
 
 
